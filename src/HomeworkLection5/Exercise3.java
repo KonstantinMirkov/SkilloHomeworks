3fdcf0dca3ccbde9799726d19ace89491f7aebb1
KonstantinMirkov <74234889+KonstantinMirkov@users.noreply.github.com>
2023-10-26 14:13:03 +0300

    Homeworks

--- src/HomeworkLection5/Exercise3.java
+++ src/HomeworkLection5/Exercise3.java
@@ -18,6 +18,14 @@ public class Exercise3 {
 
         int[][] matrix = new int[rows][columns];
 
+        System.out.println("Enter the elements of the matrix: ");
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                System.out.println("Element at position " + (row + 1) + "," + (col + 1) + ": ");
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
                 System.out.print(matrix[row][col] + " ");
